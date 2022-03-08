c0fd46df0b9433831c442c1194b9e30e1da7bd7c
Alex <zhialex.ru@gmail.com>
2022-03-08 18:12:16 +0300

    upd

--- src/test/java/com/zhialex/tests/ui_tests/tests/MainPageTest.java
+++ src/test/java/com/zhialex/tests/ui_tests/tests/MainPageTest.java
@@ -1,9 +1,15 @@
 package com.zhialex.tests.ui_tests.tests;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import com.zhialex.tests.ui_tests.pages.MainPage;
 import io.qameta.allure.Epic;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
 
 public class MainPageTest extends BaseTest {
 
@@ -14,9 +20,12 @@ public class MainPageTest extends BaseTest {
     @DisplayName("Проверка перехода к компоненту обратной связи")
     void checkScrollByNewProjectButton() {
 
-        mainPage.openPage();
-        mainPage.clickStartProjectButton();
-        mainPage.checkScroll();
+//        mainPage.openPage();
+//        mainPage.clickStartProjectButton();
+//        mainPage.checkScroll();
+        open("https://maxilect.ru/");
+        $x("//a[contains(text(),'Start your project')]").shouldBe(visible).click();
+        $x("//input[@name='user-name']").shouldBe(visible);
     }
 
     @Epic("Главная страница")
